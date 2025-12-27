40b47dfcb5de322c09756893900613b96ccd0f5d
Niols <niols@niols.fr>
2025-12-27 02:13:20 +0100

    [POC] Patch Biniou such that it works in js_of_ocaml

--- ppx_deriving_biniou/flake-part.nix
+++ ppx_deriving_biniou/flake-part.nix
@@ -3,6 +3,11 @@
     { pkgs, utils, ... }:
     let
       opkgs = pkgs.ocamlPackages;
+
+      biniou-patched = opkgs.biniou.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [ ./biniou-1.2.2.patch ];
+      });
+
     in
     {
       packages.ppx_deriving_biniou = opkgs.buildDunePackage {
@@ -13,11 +18,18 @@
           ppxlib
           ppx_import
         ];
-        propagatedBuildInputs = [ opkgs.biniou ];
+        propagatedBuildInputs = [ biniou-patched ];
         doCheck = true;
         checkInputs = with opkgs; [
           ppx_deriving_qcheck
           qcheck-alcotest
+          js_of_ocaml
+          js_of_ocaml-compiler
+        ];
+        nativeCheckInputs = with opkgs; [
+          js_of_ocaml
+          js_of_ocaml-compiler
+          pkgs.nodejs
         ];
       };
     };
