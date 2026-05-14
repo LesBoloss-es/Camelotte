ffbf2602f860df51c55abcd7175dd544af129d3d
Niols <niols@niols.fr>
2026-05-14 09:20:02 +0100

    Use `raise` rather than `Lwt.fail` to carry a backtrace

--- monadise/lib/monadise_lwt.ml
+++ monadise/lib/monadise_lwt.ml
@@ -2,5 +2,5 @@ include Monadise.Make(struct
   type 'a t = 'a Lwt.t
   let return = Lwt.return
   let bind' ~on_error x f =
-    Lwt.try_bind (fun () -> x) f (fun exn -> on_error (); Lwt.fail exn)
+    Lwt.try_bind (fun () -> x) f (fun exn -> on_error (); raise exn)
 end)
